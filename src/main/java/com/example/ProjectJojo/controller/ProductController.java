ddb8024e556e78222b5d7f86530c10902b936729
ANTUS_DEV\phama <phamantu123@gmail.com>
2025-04-12 01:50:18 +0700

    add CrossOrigin annotation to ProductController for CORS support

--- src/main/java/com/example/ProjectJojo/controller/ProductController.java
+++ src/main/java/com/example/ProjectJojo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.ProjectJojo.controller;
 import com.example.ProjectJojo.entity.Product;
 import com.example.ProjectJojo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
